ee39b9602b4dfd5843ed22279b74830c0b0598d6
Andrii Skliar <andreas.skliar@gmail.com>
2024-06-11 13:46:28 +0300

    base data

---
+++ src/app/layout.js
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Lama Dev",
+  description: "This is the description",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="container">{children}</div>
+      </body>
+    </html>
+  );
+}
